8506c32ccb3a1d543b33dda729fdb7979ab8c957
katelina7 <jocelyngau@yahoo.com>
2020-11-14 23:02:53 -0800

    fixed mobile view and map key

--- src/components/AllQuestions.js
+++ src/components/AllQuestions.js
@@ -4,16 +4,16 @@ import {useState} from 'react';
 
 const FAQs = [{question: "What is a hackathon?", answer: "This is about the length of a sentence. We want to include about 3 sentences worth of stuff to answer questions. Butter is a healthy and nutritious snack."},
               {question: "Will the hackathon happen virtually or in person?", answer: ":3"},
-              {question: ":3", answer: ":)"}]
+              {question: "What???", answer: ":)"}]
 
 const AllQuestions = props => {
     const[currentlyOpen, setOpen] = useState(-1)
 
     return(
-        <div className={styles.container}>
+        <div className={styles.container} >
             {FAQs.map((faq, i) => {
-                return <div className={styles.question}><FAQ question={faq.question} answer={faq.answer} 
-                isOpen={currentlyOpen===i} id={i} setOpen={setOpen} key={faq.question}/></div>
+                return <div className={styles.question} key={faq.question}><FAQ question={faq.question} answer={faq.answer} 
+                isOpen={currentlyOpen===i} id={i} setOpen={setOpen} /></div>
             })}
         </div>
     )
