a8f58e3124386d05e1eb028e2c07b124aa3da354
Praneer Shrestha <praneer@administrators-MacBook-Pro.local>
2020-11-15 05:52:15 -0800

    completed website mobile version

--- src/pages/MobileHome.js
+++ src/pages/MobileHome.js
@@ -2,9 +2,20 @@ import MobileHeader from '../components/MobileHeader'
 import MobileSponsors from '../components/MobileSponsors'
 import MobileTracks from '../components/MobileTracks'
 import MobileIntroduction from '../components/MobileIntroduction'
+import FAQ from '../components/AllQuestions'
+
+import styles from '../css/MobileHome.module.css'
 
 const MobileHome = props => {
-  return <>Mobile</>
+  return (
+    <div className={styles.container}>
+      <MobileHeader />
+      <MobileIntroduction />
+      <MobileTracks />
+      <FAQ />
+      <MobileSponsors />
+    </div>
+  )
 }
 
 export default MobileHome
