1f8ce67ab5ac08932cc5a79a5a5446a8e001e193
Praneer Shrestha <praneer@administrators-MacBook-Pro.local>
2020-11-15 03:28:24 -0800

    what do u know, margin left 9%

--- src/components/MobileSponsors.js
+++ src/components/MobileSponsors.js
@@ -1,13 +1,13 @@
 import styles from '../css/MobileSponsors.module.css'
 
-import sfss from '../assets/sfss-logo.svg'
+import sfss from '../assets/sfss-logo.png'
 
 const MobileSponsors = props => {
   return (
     <div className={styles.container}>
       <p className={styles.heading}>Sponsors</p>
       <div className={styles.sponsorContainer}>
-        <img src={sfss} alt="SFSS Logo" />
+        <img width="180px" src={sfss} alt="SFSS Logo" />
       </div>
     </div>
   )
