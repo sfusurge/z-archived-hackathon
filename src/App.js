0c7fca99e35204dffc40b14d03028d8c2a681575
Joshua Peng <jpa95@sfu.ca>
2020-11-15 23:59:19 -0800

    Corrected padding

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import HomeToggle from './components/HomeToggle'
 import styles from './css/App.module.css'
-
+import EventTracks from './components/EventTracks.js'
 const App = () => {
+  
   return (
     <div id={styles.app}>
       <Router>
         <Switch>
           <Route path="/">
-            <HomeToggle/>
+          <EventTracks/>
           </Route>
         </Switch>
       </Router>
