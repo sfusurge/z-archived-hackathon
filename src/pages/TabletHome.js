7a60aa6a2e8f12433baceeafc96d987984d9a66e
alikhamesy <alikhamesy0@gmail.com>
2020-11-16 03:24:51 -0800

    work in progress

---
+++ src/pages/TabletHome.js
@@ -0,0 +1,23 @@
+import MobileHeader from "../components/MobileHeader"
+import AboutContext from "../components/AboutContext"
+import EventTracks from "../components/EventTracks"
+import FaqMobile from "../components/FaqMobile"
+import MobileSponsors from "../components/MobileSponsors"
+import FooterMobile from "../components/FooterMobile"
+
+import styles from "../css/TabletHome.module.css"
+
+const TabletHome = props => {
+  return (
+    <div className={styles.container}>
+      <MobileHeader />
+      <AboutContext />
+      <EventTracks />
+      <FaqMobile />
+      <MobileSponsors />
+      <FooterMobile />
+    </div>
+  )
+}
+
+export default TabletHome
