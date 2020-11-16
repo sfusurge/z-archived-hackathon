c2ce5b1932e6013658a6506e066e0745fa6774fe
Joshua Peng <jpa95@sfu.ca>
2020-11-16 00:02:27 -0800

    Remove event tracks from app.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import HomeToggle from './components/HomeToggle'
 import styles from './css/App.module.css'
-import EventTracks from './components/EventTracks.js'
+
 const App = () => {
-  
   return (
     <div id={styles.app}>
       <Router>
         <Switch>
           <Route path="/">
-          <EventTracks/>
+            <HomeToggle/>
           </Route>
         </Switch>
       </Router>
