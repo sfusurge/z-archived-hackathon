0b3fc7f897e941deb975f53b11d997f702095983
Joshua Peng <jpa95@sfu.ca>
2020-11-16 00:33:48 -0800

    removed from app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import HomeToggle from './components/HomeToggle'
-import Event from './components/EventTracks.js'
 import styles from './css/App.module.css'
 
 const App = () => {
   return (
     <div id={styles.app}>
-      <Event />
       <Router>
         <Switch>
           <Route path="/">
