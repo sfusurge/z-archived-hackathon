1abccef6e2f1cc07097fe739a43aae6954943525
praneershrest <praneer.shrest@gmail.com>
2020-12-20 20:39:37 -0800

    adds links to sponsor images

--- src/utils/sponsorsList.js
+++ src/utils/sponsorsList.js
@@ -3,37 +3,37 @@ const sponsors = [
     src: './sfss-logo.png',
     name: 'Simon Fraser Student Society',
     width: '200px',
-    link: ''
+    link: 'https://sfss.ca/'
   },
   {
     src: './fair-trade-logo.png',
     name: 'SFU FairTrade',
     width: '150px',
-    link: ''
+    link: 'http://www.sfu.ca/fairtrade.html'
   },
   {
     src: './wics-logo.png',
     name: 'SFU Women in Computing Science',
     width: '205px',
-    link: ''
+    link: 'https://www.sfu.ca/computing/wics.html'
   },
   {
     src: './mlh-logo.png',
     name: 'Major League Hacking',
     width: '130px',
-    link: ''
+    link: 'https://mlh.io/'
   },
   {
     src: './github-logo.png',
     name: 'GitHub',
     width: '150px',
-    link: ''
+    link: 'https://github.com/'
   },
   {
     src: './sticker-mule-logo.png',
     name: 'StickerMule',
     width: '230px',
-    link: ''
+    link: 'https://www.stickermule.com/ca'
   }
 ]
 
