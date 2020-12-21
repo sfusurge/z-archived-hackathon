65e7dbb084c585da224760ac57188322cd78a2cb
Joshua Peng <jpa95@sfu.ca>
2020-12-20 16:01:37 -0800

    Update ExecMember.js

--- src/components/ExecMember.js
+++ src/components/ExecMember.js
@@ -1,27 +1,24 @@
 import styles from '../css/ExecMember.module.css'
 const ExecMember = ({ name, src, width, height, link, customStyle }) => {
-  console.log(customStyle)
   return (
-    <>
-      <a
-        className={styles.link}
-        style={customStyle}
-        target='_blank'
-        rel='noreferrer'
-        href={link}
-      >
-        <div className={styles.container}>
-          <img
-            className={styles.img}
-            src={src}
-            width={width}
-            height={height}
-            alt={`Logo of ${name}`}
-          />
-          <p className={styles.name}>{name}</p>
-        </div>
-      </a>
-    </>
+    <a
+      className={styles.link}
+      style={customStyle}
+      target="_blank"
+      rel="noreferrer"
+      href={link}
+    >
+      <div className={styles.container}>
+        <img
+          className={styles.img}
+          src={src}
+          width={width}
+          height={height}
+          alt={`Image of ${name}`}
+        />
+        <p className={styles.name}>{name}</p>
+      </div>
+    </a>
   )
 }
 
