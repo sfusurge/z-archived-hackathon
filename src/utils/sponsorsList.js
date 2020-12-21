87865b6c62cace99f9af93ec34b89849fcb17a35
Joshua Peng <jpa95@sfu.ca>
2020-12-20 17:04:55 -0800

    added images

---
+++ src/utils/sponsorsList.js
@@ -0,0 +1,32 @@
+const sponsors = [
+  {
+    src: './sponsors/sfss-logo.png',
+    name: 'SFSS',
+    link: ''
+  },
+  {
+    src: './sponsors/fair-trade-logo.png',
+    name: 'FairTrade',
+    link: ''
+  },
+  {
+    src: './sponsors/wics-logo.png',
+    name: 'WICS',
+    link: ''
+  },
+  {
+    src: './sponsors/mlh-logo.png',
+    name: 'MLH',
+    link: ''
+  },
+  {
+    src: './sponsors/github-logo.png',
+    name: 'Github',
+    link: ''
+  },
+  {
+    src: './sponsors/sticker-mule-logo.png',
+    name: 'StickerMule',
+    link: ''
+  }
+]
