dff4501adfe916006000ab934ed8a963e072d48d
Joshua Peng <jpa95@sfu.ca>
2020-12-31 15:37:26 -0800

    Align grid

--- src/components/PastEvents.js
+++ src/components/PastEvents.js
@@ -16,24 +16,67 @@ const PastEvents = props => {
       </div>
       <div className={styles.body}>
         <div className={styles.menu}>
-          <p>View by: Date taken</p>
-          <div className={styles.gridBtn}> </div>
+          <p className={styles.viewBy}>View by: Date taken</p>
+          <div className={styles.gridBtn}>
+            <div className={styles.gridIcon} />
+            <span className={styles.orangeOutline}></span>
+          </div>
         </div>
         <p className={styles.date}>2020</p>
         <div className={styles.images2020}>
-          <img className={styles.img1} src={imageResolver('./2020_1.png').default} alt="image1" />
-          <img className={styles.img2} src={imageResolver('./2020_2.png').default} alt="image2" />
-          <img className={styles.img3} src={imageResolver('./2020_3.png').default} alt="image3" />
-          <img className={styles.img4} src={imageResolver('./2020_4.png').default} alt="image4" />
-          <img className={styles.img5} src={imageResolver('./2020_5.png').default} alt="image5" />
+          <img
+            className={styles.img1}
+            src={imageResolver('./2020_1.png').default}
+            alt="image1"
+          />
+          <img
+            className={styles.img2}
+            src={imageResolver('./2020_2.png').default}
+            alt="image2"
+          />
+          <img
+            className={styles.img3}
+            src={imageResolver('./2020_3.png').default}
+            alt="image3"
+          />
+          <img
+            className={styles.img4}
+            src={imageResolver('./2020_4.png').default}
+            alt="image4"
+          />
+          <img
+            className={styles.img5}
+            src={imageResolver('./2020_5.png').default}
+            alt="image5"
+          />
         </div>
         <p className={styles.date}>2019</p>
         <div className={styles.images2019}>
-          <img className={styles.img1_19} src={imageResolver('./2019_1.png').default} alt="image1" />
-          <img className={styles.img2_19} src={imageResolver('./2019_2.png').default} alt="image2" />
-          <img className={styles.img3_19} src={imageResolver('./2019_3.png').default} alt="image3" />
-          <img className={styles.img4_19} src={imageResolver('./2019_4.png').default} alt="image4" />
-          <img className={styles.img5_19} src={imageResolver('./2019_5.png').default} alt="image5" />
+          <img
+            className={styles.img1_19}
+            src={imageResolver('./2019_1.png').default}
+            alt="image1"
+          />
+          <img
+            className={styles.img2_19}
+            src={imageResolver('./2019_2.png').default}
+            alt="image2"
+          />
+          <img
+            className={styles.img3_19}
+            src={imageResolver('./2019_3.png').default}
+            alt="image3"
+          />
+          <img
+            className={styles.img4_19}
+            src={imageResolver('./2019_4.png').default}
+            alt="image4"
+          />
+          <img
+            className={styles.img5_19}
+            src={imageResolver('./2019_5.png').default}
+            alt="image5"
+          />
         </div>
       </div>
     </div>
