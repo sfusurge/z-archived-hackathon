49a6d371b01cf555f9de5ef407ac4afa853fa3e6
Joshua Peng <jpa95@sfu.ca>
2021-01-04 17:44:30 -0800

    Cleaned up code

--- src/components/SideBarEvent.js
+++ src/components/SideBarEvent.js
@@ -3,7 +3,7 @@ const SideBarEvents = props => {
   return (
     <div className={styles.container}>
       <p className={styles.date}>{props.date}</p>
-      <span className={styles.line}></span>
+      <span className={styles.line} />
     </div>
   )
 }
