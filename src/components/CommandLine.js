2d55c000eca7c81881a05aab75602fa0e72d5785
alikhamesy <alikhamesy0@gmail.com>
2021-01-13 01:17:24 -0700

    forgot one woops

--- src/components/CommandLine.js
+++ src/components/CommandLine.js
@@ -47,54 +47,50 @@ const CommandLine = () => {
         <Typist.Delay ms={200} />
         <span className={styles.fontAnimated}> . </span>
         <br />
-<<<<<<< HEAD
-      <span className={styles.fontAnimated}>Opening application</span>
-=======
-        <span className={styles.fontAnimated}>Opening Application</span>
->>>>>>> dev
-      <Typist.Delay ms={200} />
-      <span className={styles.fontAnimated}> . </span>
-      <Typist.Delay ms={200} />
-      <span className={styles.fontAnimated}> . </span>
-      <Typist.Delay ms={200} />
-      <span className={styles.fontAnimated}> . </span>
-      <Typist.Delay ms={200} />
-      <span className={styles.fontAnimated}> . </span>
+        <span className={styles.fontAnimated}>Opening application</span>
+        <Typist.Delay ms={200} />
+        <span className={styles.fontAnimated}> . </span>
+        <Typist.Delay ms={200} />
+        <span className={styles.fontAnimated}> . </span>
+        <Typist.Delay ms={200} />
+        <span className={styles.fontAnimated}> . </span>
+        <Typist.Delay ms={200} />
+        <span className={styles.fontAnimated}> . </span>
       </Typist>
 
       {
-    secondMsg && (
-      <Typist
-        className={styles.typist}
-        avgTypingDelay={1}
-        stdTypingDelay={10}
-        cursor={{
-          show: true,
-          blink: false,
-          element: '▌',
-          hideWhenDone: false
-        }}
-      >
-        <br />
-        <br />
-        <Typist.Delay ms={500} />
-        <span className={styles.fontAnimated}>
-          If the application form has not appeared, please make sure that
-          popups are enabled or click the following link:
+        secondMsg && (
+          <Typist
+            className={styles.typist}
+            avgTypingDelay={1}
+            stdTypingDelay={10}
+            cursor={{
+              show: true,
+              blink: false,
+              element: '▌',
+              hideWhenDone: false
+            }}
+          >
+            <br />
+            <br />
+            <Typist.Delay ms={500} />
+            <span className={styles.fontAnimated}>
+              If the application form has not appeared, please make sure that
+              popups are enabled or click the following link:
           </span>
-        <br />
-        <a
-          href="https://stormhacks.typeform.com/to/GWfDap3w"
-          target="_blank"
-          rel="noreferrer"
-          className={styles.linkAnimated}
-        >
-          https://stormhacks.typeform.com/to/GWfDap3w
+            <br />
+            <a
+              href="https://stormhacks.typeform.com/to/GWfDap3w"
+              target="_blank"
+              rel="noreferrer"
+              className={styles.linkAnimated}
+            >
+              https://stormhacks.typeform.com/to/GWfDap3w
           </a>
-        <br />
-      </Typist>
-    )
-  }
+            <br />
+          </Typist>
+        )
+      }
     </div >
   )
 }
