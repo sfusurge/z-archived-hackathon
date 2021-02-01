3f0a2bd57c158179644292f1c6e88f0f4a65d2c6
Joshua Peng <jpa95@sfu.ca>
2021-01-31 18:58:52 -0800

    trimmed imgs

--- src/utils/sponsorsList.js
+++ src/utils/sponsorsList.js
@@ -3,26 +3,53 @@ const sponsors = [
     srcLight: './sfss-logo.png',
     name: 'Simon Fraser Student Society',
     width: '500px',
+    title: 'goldSponsors',
     link: 'https://sfss.ca/'
+  },
+  {
+    srcLight: './sfualumni-logo.png',
+    name: 'SFU Alumni Association',
+    title: 'silverSponsors',
+    width: '300px',
+    link: 'https://mlh.io/'
+  },
+  {
+    srcLight: './fortisBC-logo-light.png',
+    srcDark: './fortisBC-logo-dark.png',
+    name: 'Fortis BC',
+    title: 'silverSponsors',
+    width: '300px',
+    link: 'https://mlh.io/'
   },
   {
     srcLight: './mlh-logo-light.png',
     srcDark: './mlh-logo-dark.png',
     name: 'Major League Hacking',
-    width: '250px',
+    title: 'silverSponsors',
+    width: '200px',
     link: 'https://mlh.io/'
   },
   {
     srcLight: './fair-trade-logo-light.png',
     srcDark: './fair-trade-logo-dark.png',
     name: 'SFU FairTrade',
+    title: 'silverSponsors',
+    width: '120px',
+    link: 'http://www.sfu.ca/fairtrade.html'
+  },
+  {
+    srcLight: './guusto-logo-light.png',
+    srcDark: './guusto-logo-dark.png',
+    name: 'Guusto',
+    title: 'silverSponsors',
     width: '150px',
     link: 'http://www.sfu.ca/fairtrade.html'
   },
   {
     srcLight: './wics-logo.png',
     name: 'SFU Women in Computing Science',
-    width: '180px',
+    width: '160px',
+    title: 'silverSponsors',
     link: 'https://www.sfu.ca/computing/wics.html'
   },
   {
@@ -30,6 +57,7 @@ const sponsors = [
     srcDark: './ssss-logo-dark.png',
     name: 'SFU Software Systems Student Society',
     width: '130px',
+    title: 'bronzeSponsors',
     link: 'https://ssss-sfu.github.io/'
   },
   {
@@ -37,6 +65,7 @@ const sponsors = [
     srcDark: './chang-institute-logo-dark.png',
     name: 'Chang institute',
     width: '200px',
+    title: 'bronzeSponsors',
     link: 'https://www.sfu.ca/changinstitute.html'
   },
   {
@@ -44,6 +73,7 @@ const sponsors = [
     srcDark: './compass-program-logo-dark.png',
     name: 'Compass Program',
     width: '130px',
+    title: 'bronzeSponsors',
     link:
       'https://www.sfu.ca/changinstitute/events---outreach/compass-program0.html'
   },
@@ -52,6 +82,7 @@ const sponsors = [
     srcDark: './hedgehog-logo-dark.png',
     name: 'Hedgehog technologies',
     width: '200px',
+    title: 'bronzeSponsors',
     link: 'https://www.hedgehogtech.com/'
   },
   {
@@ -59,6 +90,15 @@ const sponsors = [
     srcDark: './echoAR-logo-dark.png',
     name: 'echoAR',
     width: '180px',
+    title: 'bronzeSponsors',
+    link: 'https://www.echoar.xyz/'
+  },
+  {
+    srcLight: './codex-logo-light.png',
+    srcDark: './codex-logo-dark.png',
+    name: 'The Codex',
+    width: '180px',
+    title: 'bronzeSponsors',
     link: 'https://www.echoar.xyz/'
   },
   {
@@ -66,18 +106,29 @@ const sponsors = [
     srcDark: './fluttervalsad-logo-dark.png',
     name: 'Flutter Valsad',
     width: '180px',
+    title: 'bronzeSponsors',
     link: 'https://fluttervalsad.com/'
+  },
+  {
+    srcLight: './hackhub-logo-light.png',
+    srcDark: './hackhub-logo-dark.png',
+    name: 'Hackhub',
+    title: 'silverSponsors',
+    width: '150px',
+    link: 'http://www.sfu.ca/fairtrade.html'
   },
   {
     srcLight: './github-logo.png',
     name: 'GitHub',
     width: '130px',
+    title: 'bronzeSponsors',
     link: 'https://github.com/'
   },
   {
     srcLight: './sticker-mule-logo.png',
     name: 'StickerMule',
     width: '200px',
+    title: 'bronzeSponsors',
     link: 'http://hackp.ac/mlh-stickermule-hackathons'
   }
 ]
