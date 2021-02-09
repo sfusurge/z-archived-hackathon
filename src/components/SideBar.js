1edb7cdd4f6ddd1d20534c14c089a2199e8bcfa5
Joshua Peng <jpa95@sfu.ca>
2021-02-08 19:28:10 -0800

    Added Pst

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -9,7 +9,7 @@ const workshops = [
       {
         img: 'react',
         eventName: 'REACT201: Intro to React and Hooks',
-        eventTime: 'Zoom @ 5:30-7:00pm',
+        eventTime: 'Zoom @ 5:30-7:00pm PST',
         link: 'https://lu.ma/wk8cwah3'
       }
     ]
@@ -20,13 +20,13 @@ const workshops = [
       {
         img: 'echo',
         eventName: 'How to Build a Cloud-Connected AR/VR App',
-        eventTime: 'YouTube @ 10:00-10:45am',
+        eventTime: 'YouTube @ 10:00-10:45am PST',
         link: 'https://www.youtube.com/watch?v=Uo2z8oKzNGk&ab_channel=echoAR'
       },
       {
         img: 'android',
         eventName: 'DEV101M: Intro to Android Studio',
-        eventTime: 'Zoom @ 1:00-2:30pm',
+        eventTime: 'Zoom @ 1:00-2:30pm PST',
         link: 'https://lu.ma/bmcnugtm'
       }
     ]
